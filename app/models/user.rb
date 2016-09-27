78ca358e6a78928d151c515d994ee5e9e840b8c9
kevin <kevinochieng548@gmail.com>
2016-09-27 23:03:05 +0300

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
-  validates :f_name, length: {minimum: 3}
-  validates :l_name, length: {minimum: 3}
+  validates :f_name,presence: true, length: {minimum: 3}
+  validates :l_name,presence: true,  length: {minimum: 3}
+
+  has_secure_password
+  validates :password, length: {minimum: 5}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true,length: {maximum: 100},
+          format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
