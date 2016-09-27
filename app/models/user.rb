0fbfeab7b2dde1feab0a6eb8e85144054f27ae7a
kevin <kevinochieng548@gmail.com>
2016-09-27 22:23:53 +0300

    add @user instance variable to users new action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :f_name, length: {minimum: 3}
+  validates :l_name, length: {minimum: 3}
 end
--- db/migrate/20160927191557_create_users.rb
+++ db/migrate/20160927191557_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :f_name
+      t.string :l_name
       t.string :email
 
       t.timestamps
