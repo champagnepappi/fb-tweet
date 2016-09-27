06f1ffae8b18056b84ae38b9049a70b89dfa07d1
kevin <kevinochieng548@gmail.com>
2016-09-27 12:35:22 +0300

    test title helper

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @common_title = "| FbTweet"
+  end
   test "should get home" do
     get root_path
     assert_response :success
+    assert_select 'title', "Home #{@common_title}"
   end
 
 end
