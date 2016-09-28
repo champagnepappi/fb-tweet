4acddd3a5f1c80672ec154ff9a4fa22c71103e25
kevin <kevinochieng548@gmail.com>
2016-09-28 11:59:30 +0300

    generate migration for password digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927191557) do
+ActiveRecord::Schema.define(version: 20160928085656) do
 
   create_table "users", force: :cascade do |t|
     t.string   "f_name"
     t.string   "l_name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
