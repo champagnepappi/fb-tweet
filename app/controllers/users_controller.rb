a42ce3d9d3c4f2acabd5902adf85a59abc62ba9f
kevin <kevinochieng548@gmail.com>
2016-09-28 11:49:29 +0300

    define user_params private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+  end
+
   def show
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:f_name,:l_name,:email,:password,:password_confirmation)
+  end
 end
+
