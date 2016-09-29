f5f35a0728fb053b83f30b9d0577c781a796e5b4
kevin <kevinochieng548@gmail.com>
2016-09-29 09:08:16 +0300

    setup user in users controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,16 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:lesley)
+  end
   test "should get new" do
     get new_user_path
     assert_response :success
   end
 
   test "should get show" do
-    get user_path
+    get user_path(@user)
     assert_response :success
   end
 
