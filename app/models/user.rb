7fa5a9648c1901b2423219fde2bc4ff7a67dc8d0
kevin <kevinochieng548@gmail.com>
2016-09-30 21:59:57 +0300

    modify class method User.digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 
 
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min cost ? BCrypt::Engine::MIN_COST :
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
