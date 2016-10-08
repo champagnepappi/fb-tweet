d8fb4d43745e1b26ab090ed3292bb50da12badb3
kevin <kevinochieng548@gmail.com>
2016-10-08 16:03:29 +0300

    define remember method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   validates :f_name,presence: true, length: {minimum: 3}
   validates :l_name,presence: true,  length: {minimum: 3}
 
@@ -18,4 +19,10 @@ class User < ApplicationRecord
   def User.new_token
     SecureRandom.urlsafe_base64
   end
+
+  def remember
+   self.remember_token = User.new_token 
+   update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
 end
