04a6635e77007f2c657d1b4050d02ddad415d436
kevin <kevinochieng548@gmail.com>
2016-10-14 15:46:02 +0300

    forget persistent session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,4 +29,10 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 end
