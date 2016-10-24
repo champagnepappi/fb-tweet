b0354c4b74ac9ef25ee64686517a4bfe5133dedc
kevin <kevinochieng548@gmail.com>
2016-10-24 13:24:42 +0300

    test authenticated for nonexistent digest

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,8 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: "Cuty Pie", email: "elsy@gmail.com",
                      password: "justme",password_confirmation: "justme")
   end
+
+  test "authenticated? should return false if digest is nil" do
+    assert_not @user.authenticated?('')
+  end
 end
