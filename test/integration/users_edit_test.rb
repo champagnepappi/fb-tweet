31e969b39a75983797033d737971f6ce6e2f2521
kevin <kevinochieng548@gmail.com>
2016-11-01 08:44:24 +0300

    test successful edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -18,4 +18,18 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     }
     assert_template 'users/edit'
   end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: {
+      user: {
+         f_name: "mark",
+         l_name: "Zuckerberg",
+         password: "",
+         password_confirmation: ""
+      }
+    }
+    assert_redirected_to @user
+  end
 end
