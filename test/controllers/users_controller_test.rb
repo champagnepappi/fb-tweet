a9dbbed2584c893af899d0a82722dafe375f3bc2
kevin <kevinochieng548@gmail.com>
2016-11-04 13:30:32 +0300

    log in user in test to get new and show

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,11 +5,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user = users(:lesley)
   end
   test "should get new" do
+    log_in_as(@user)
     get new_user_path
     assert_response :success
   end
 
   test "should get show" do
+    log_in_as(@user)
     get user_path(@user)
     assert_response :success
   end
