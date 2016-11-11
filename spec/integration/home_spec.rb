f9c16c6150a68ead86b3e2422f7c0367879d6c08
kevin <kevinochieng548@gmail.com>
2016-11-11 20:42:40 +0300

    test sign in

--- spec/integration/home_spec.rb
+++ spec/integration/home_spec.rb
@@ -8,3 +8,29 @@ describe 'home page' do
     end
   end
 end
+
+# describe 'signup process' do
+#   it "signup user with valid credentials" do
+#     visit(new_user_path)
+#     page.fill_in "First name", with: "Donald"
+#     page.fill_in "Last name", with: "Trump"
+#     page.fill_in "email", with: "don@gmail.com"
+#     page.fill_in "Password", with: "pass"
+#     page.fill_in "Password confirmation", with: "pass"
+#     click_button('Create an Account')
+#     page.should_content("Welcome to FbTweet")
+#   end
+# end
+#
+describe 'sign in process' do
+  before :each do
+    User.create(email: "somebody@gmail.com", password: 'password')
+  end
+
+  it 'signs me in' do
+    visit '/login'
+      fill_in 'Email', with: 'somebody@gmail.com'
+      fill_in 'Password', with: 'password'
+    click_button 'Submit'
+  end
+end
--- spec/integration/views.rb
+++ spec/integration/views.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
-describe 'titles' do
+describe 'page titles' do
   it "should have page title" do
     visit('/')
     page.should have_selector("title", text: "FbTweet")
   end
+
+  it 'should signup user' do
+    visit('/')
+    expect(page).to have_content('Create an Account')
+    click_button('Create an Account')
+  end
 end
+
