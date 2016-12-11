744802d4f4a675ccfcea2ac3daeab31d7c9fbd18
kevin <kevinochieng548@gmail.com>
2016-12-11 22:59:33 +0300

    use generalized authenticated method in user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,6 +7,6 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "authenticated? should return false if digest is nil" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 end
