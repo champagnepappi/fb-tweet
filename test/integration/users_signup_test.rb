8e6aec6aeadc4a99239d0354a446e516a151b1e8
kevin <kevinochieng548@gmail.com>
2016-12-27 00:54:47 +0300

    test account activation with valid token

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -47,5 +47,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     #valid token wrong email
     get edit_account_activation_path(user.activation_token, email: 'wrong')
     assert_not is_logged_in?
+    #valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
