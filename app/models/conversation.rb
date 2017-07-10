a3bf4272a9ddb6e464365b8501b656d8ee2e0a9f
kevin <kevinochieng548@gmail.com>
2017-07-10 15:46:16 +0300

    define scope between

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -8,4 +8,9 @@ class Conversation < ApplicationRecord
   scope :involving, -> (user) do
     where("conversations.sender_id =? OR conversations.recipient_id=?", user.id, user.id)
   end
+   
+  scope :between, -> (sender_id, recepient_id) do
+    where("(conversations.sender_id =? AND conversations.recipient_id = ?) OR
+          (conversations.sender_id =? AND conversations.recipient_id =?)" ,sender_id, recipient_id, recipient_id, sender_id)
+  end
 end
