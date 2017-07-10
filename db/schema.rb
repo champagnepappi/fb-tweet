7e379d27e2a29ab054fb1a945706789c4f60848a
kevin <kevinochieng548@gmail.com>
2017-07-10 15:13:05 +0300

    migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116035545) do
+ActiveRecord::Schema.define(version: 20170710120742) do
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "f_name"
