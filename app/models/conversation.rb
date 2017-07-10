abe714de424065490ecbfef60c66625b70338435
kevin <kevinochieng548@gmail.com>
2017-07-10 15:51:57 +0300

    add comments

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -5,10 +5,12 @@ class Conversation < ApplicationRecord
 
   validates_uniqueness_of :sender_id, :scope => :recipient_id
 
+  #retrieve all conversations of currently-logged in user
   scope :involving, -> (user) do
     where("conversations.sender_id =? OR conversations.recipient_id=?", user.id, user.id)
   end
    
+  #check if a conversation exists between any given two users before we create the conversation
   scope :between, -> (sender_id, recepient_id) do
     where("(conversations.sender_id =? AND conversations.recipient_id = ?) OR
           (conversations.sender_id =? AND conversations.recipient_id =?)" ,sender_id, recipient_id, recipient_id, sender_id)
