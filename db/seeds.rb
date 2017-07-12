26d12f35f781d25584f5f29e676ca3836187f903
kevin <kevinochieng548@gmail.com>
2017-07-12 15:09:21 +0300

    seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ User.create(
+  f_name: "Kevin",
+  l_name: "Santos",
+  email: "kevinsantos@gmail.com",
+  password: "justpass",
+  password_confirmation: "justpass"
+)
