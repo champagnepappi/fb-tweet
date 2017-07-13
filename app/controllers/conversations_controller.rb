a40fdea5cb20499111cfc753d1b0e48f97ec86ee
kevin <kevinochieng548@gmail.com>
2017-07-13 15:33:29 +0300

    define conversation create action

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,2 +1,9 @@
 class ConversationsController < ApplicationController
+  def create
+    if Conversation.between(params[:sender_id], params[:recipient_id]).present?
+      @conversation = Conversation.between(params[:sender_id, params[:recipient_id]]).first
+    else
+      @conversatiom = Conversation.create(conversation_params)
+    end
+  end
 end
