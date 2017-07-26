b77f20ad3532e408bcc17dba8c4aa7fd91637aed
kevin <kevinochieng548@gmail.com>
2017-07-26 11:48:46 +0300

    define video create action

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,6 +2,16 @@ class VideosController < ApplicationController
   def new
   end
 
+  def create
+    @video = current_user.videos.build(video_params)
+    if @video.save
+      redirect_to current_user
+    else
+      redirect_to root_path
+      flash[:danger] = "There was a problem posting the video"
+    end
+  end
+
   def show
   end
 end
