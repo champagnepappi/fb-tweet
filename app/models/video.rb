155c2dd7ef70c4ab831c9baad1ff67c1ab838d1b
kevin <kevinochieng548@gmail.com>
2017-07-27 11:14:25 +0300

    call video_size method using validate

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,8 @@ class Video < ApplicationRecord
   mount_uploader :video, VideoUploader
   belongs_to :user
 
+  validate :video_size
+
   # has_attached_file :video,
   #       styles: lambda { |a| a.instance.is_image? ? {:small => "x200>", :medium => "x300>", :large => "x400>"}  : {:thumb => { :geometry => "100x100#", :format => 'jpg', :time => 10 }, :medium => { :geometry => "300x300#", :format => 'jpg', :time => 10 }} },
   #           processors: lambda { |a| a.is_video? ? [ :ffmpeg  ] : [ :thumbnail  ]  }
