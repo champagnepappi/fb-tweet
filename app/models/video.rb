1974d7e1e436ab8ca758f1a888c13aaaf153bfbf
kevin <kevinochieng548@gmail.com>
2017-07-27 11:11:02 +0300

    define private method video size

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,4 +5,12 @@ class Video < ApplicationRecord
   # has_attached_file :video,
   #       styles: lambda { |a| a.instance.is_image? ? {:small => "x200>", :medium => "x300>", :large => "x400>"}  : {:thumb => { :geometry => "100x100#", :format => 'jpg', :time => 10 }, :medium => { :geometry => "300x300#", :format => 'jpg', :time => 10 }} },
   #           processors: lambda { |a| a.is_video? ? [ :ffmpeg  ] : [ :thumbnail  ]  }
+
+  private
+
+  def video_size
+    if video.size > 10.megabytes
+      errors.add(:video, "Should be less than 10MB")
+    end
+  end
 end
