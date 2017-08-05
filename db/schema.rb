a8358f0707d139d3ba2dea2646e55709a3f875bb
kevin <kevinochieng548@gmail.com>
2017-08-05 22:43:13 +0300

    migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170726093436) do
+ActiveRecord::Schema.define(version: 20170804191016) do
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
