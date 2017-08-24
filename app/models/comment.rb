b2b355f5e91007db237d7b14e73def9bbeae6f35
kevin <kevinochieng548@gmail.com>
2017-08-24 15:48:59 +0300

    add comments validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :video
+
+  validates :content, presence: true
+  validates :user_id, presence: true
+  validates :video_id, presence: true
 end
