c0595683cfdb281ddfd61eae6a323b8fa05caf2f
kevin <kevinochieng548@gmail.com>
2017-08-24 15:55:07 +0300

    modify comments params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:comment, :user_id, :video_id)
+    params.require(:comment).permit(:content, :user_id, :video_id)
   end
 end
