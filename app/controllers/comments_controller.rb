9026a3c016f49550a07a0f4f0fa62ed93b2c125e
kevin <kevinochieng548@gmail.com>
2017-08-28 12:54:03 +0300

    define private method correct user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,4 +18,9 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content, :user_id, :video_id)
   end
+
+  def correct_user
+    @comment = Comment.find_by(id: params[:id])
+    redirect_to root_url if @comment.nil
+  end
 end
