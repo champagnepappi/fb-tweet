085b0e01ee06d339c174bbe52b6aa36f5a851f31
kevin <kevinochieng548@gmail.com>
2017-09-17 23:06:03 +0300

    test successful comment creation

--- test/integration/comment_creation.rb
+++ test/integration/comment_creation.rb
@@ -5,4 +5,18 @@ class CommentCreationTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:lesley)
   end
+  
+  test "test successful comment creation" do
+    log_in_as(@user)
+    assert_difference 'Comment.count' do
+      post comments_path, params: {
+        comment: {
+          content: "hioufhsiohfsoihshso"
+
+        }
+      }
+      
+    end
+  end
+
 end
